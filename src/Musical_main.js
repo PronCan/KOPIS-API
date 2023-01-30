4633d1f64ef266912a07b4c072b9a27dab145fe8
PronCan <twojini98@gmail.com>
2023-01-31 03:20:25 +0900

    주간 연극 랭킹 추가, 뮤지컬 추가, 연극/뮤지컬 나눠보려고 해둠

--- src/Musical_main.js
+++ src/Musical_main.js
@@ -21,6 +21,7 @@ const xml2json = require('node-xml2json');
 const Musical_main = () => {
   const [isPending, startTransition] = useTransition();
   var apiurl = 'http://localhost:5000/mu_api';
+  var rank_apiurl = 'http://localhost:5000/get_rank'
     // 공연목록 
   // const t_url = 'http://www.kopis.or.kr/openApi/restful/pblprfr'
   // + '?service=3e0f7775aa2a40238ae5d390ad13362c'
@@ -31,6 +32,7 @@ const Musical_main = () => {
   // + '&prfstate=02'; // 공연중
   // const apiurl = encodeURI(t_url);
   var [_data, setData] = useState([]);
+  var [_rank, setRank] = useState([]);
 
   // 뮤지컬 목록
 useEffect(()=>{
@@ -46,6 +48,22 @@ useEffect(()=>{
   }
   getData();
 },[])
+
+  // 연극 랭킹
+  useEffect(()=>{
+    function getData() {
+      axios.get(rank_apiurl)
+        .then(res => {
+           startTransition(async()=>{
+            var _json = await xml2json.parser(res.data);
+            console.log('연극랭킹', _json);
+            setRank(_json.boxofs.boxof);
+           
+          });
+        })
+    }
+    getData();
+  },[])
 
 // useEffect(()=>{
 //   function getData() {
@@ -69,7 +87,18 @@ console.log("_json", _data);
     return _data&&_data.map((content, idx) => (
       <SwiperSlide key={idx} className="mySwiper-mv-slide">
         <img src={content.poster}></img>
-        {/* <p>{content}</p> */}
+        <p>{content.prfnm}</p>
+      </SwiperSlide>
+    ))
+  }
+
+  function setRankContent() {
+    return _rank&&_rank.map((content, idx) => (
+      <SwiperSlide key={idx} className="mySwiper-mv-slide">
+        {/* <div>{JSON.stringify(content)}</div> */}
+        <img src={`http://www.kopis.or.kr${content.poster}`} />
+        <p>{content.rnum}위</p>
+        <p>{content.prfnm}</p>
       </SwiperSlide>
     ))
   }
@@ -79,28 +108,44 @@ console.log("_json", _data);
     <div className='mv-logo'>
       <img src={logo}></img>
     </div>
+    <ul className='select-th-mv'>
+      <li><button>연극</button></li>
+      <li><button>뮤지컬</button></li>
+    </ul>
     <div className='mv-content-list'>
+      <span className='top-text'>주간 연극 랭킹 &nbsp;&nbsp;&nbsp;&nbsp;{isPending ? 'Loading.....' : null}</span>
       <div className='first-swiper'>
-          <Swiper
+          {/* <Swiper
             navigation={true}
             modules={[Navigation,Autoplay]}
             // mousewheel={true}
             loop={true} 
             autoplay={{ delay: 2000 }}
             className="mainSwiper">
-            {/* <SwiperSlide>
-              <img className='main' src="https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000086/86072/86072210859_727.jpg" alt='1' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img className='main' src="https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000086/86072/86072210856_727.jpg" alt='1' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img className='main' src="https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000086/86072/86072210854_727.jpg" alt='1' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img className='main' src="https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000086/86729/86729211579_727.jpg" alt='1' />
-            </SwiperSlide> */}
-          </Swiper>
+            {
+              setRankContent()
+            }
+          </Swiper> */}
+          <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 10,
+            stretch: 0,
+            depth: 120,
+            modifier: 2,
+            slideShadows: true,
+          }}
+          mousewheel={true}
+          modules={[EffectCoverflow, Pagination, Mousewheel]}
+          className="mySwiper-th-rank"
+        >
+            {
+              setRankContent()
+            }
+        </Swiper>
       </div>
       <span className='mid-text'>Now Showing &nbsp;&nbsp;&nbsp;&nbsp;{isPending ? 'Loading.....' : null}</span>
       <div className='second-swiper'>
@@ -108,7 +153,7 @@ console.log("_json", _data);
           effect={"coverflow"}
           grabCursor={true}
           centeredSlides={true}
-          slidesPerView={4}
+          slidesPerView={7}
           coverflowEffect={{
             rotate: 10,
             stretch: 0,
--- src/musical/getxml.js
+++ src/musical/getxml.js
@@ -3,21 +3,69 @@ const router = express.Router();
 const request = require('request');
 
 // const t_url = 'http://www.kopis.or.kr/openApi/restful/pblprfr?service=3e0f7775aa2a40238ae5d390ad13362c&stdate=20230101&eddate=20230228&cpage=1&rows=10&prfstate=02';
-// 공연목록 
+// 공연목록 - 뮤지컬
 const t_url = 'http://www.kopis.or.kr/openApi/restful/pblprfr'
 + '?service=3e0f7775aa2a40238ae5d390ad13362c'
 // + '&stdate=20230101'
 // + '&eddate=20230228'
-+ '&cpage=1'
-+ '&rows=10'
-+ '&prfstate=02'; // 공연중
++ '&cpage=2'
++ '&rows=20'
++ '&prfstate=02' // 공연중
++ '&shcate=GGGA'// 장르코드: 뮤지컬
+
 const url = encodeURI(t_url);
 console.log(url);
+// 공연목록 - 연극
+const t_url2 = 'http://www.kopis.or.kr/openApi/restful/pblprfr'
++ '?service=3e0f7775aa2a40238ae5d390ad13362c'
+// + '&stdate=20230101'
+// + '&eddate=20230228'
++ '&cpage=1'
++ '&rows=20'
++ '&prfstate=02'
++ '&shcate=AAAA'// 장르코드: 연극
+
+const url2 = encodeURI(t_url2);
+console.log(url2);
+
+// 예매상황판
+const t_url3 = 'http://kopis.or.kr/openApi/restful/boxoffice'
++ '?service=3e0f7775aa2a40238ae5d390ad13362c'
++ '&ststype=week'   // 주별, 월별, 일별 가능 (month/week/day)
++ '&date=20230130'
++ '&catecode=AAAA' // 장르코드 연극
+// + '&area=11' //서울
+const url3 = encodeURI(t_url3);
+console.log(url3);
 
 router.get('/mu_api', (req, res) => {
     request(
         {
             url: url,
+            method: "GET",
+        },
+        (error, response, body) => {
+            res.send(body);
+        }
+    )
+})
+
+router.get('/theater_api', (req, res) => {
+    request(
+        {
+            url: url2,
+            method: "GET",
+        },
+        (error, response, body) => {
+            res.send(body);
+        }
+    )
+})
+
+router.get('/get_rank', (req, res) => {
+    request(
+        {
+            url: url3,
             method: "GET",
         },
         (error, response, body) => {
